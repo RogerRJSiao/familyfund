2b777643fc860ca037d67a7b79eb9fe491cf82f3
RogerRJSiao <rogersiaoaiot@gmail.com>
2025-03-09 20:46:56 +0800

    feature: Add pause cmd to show printout of current state.

--- review_mismatching.bat
+++ review_mismatching.bat
@@ -6,4 +6,7 @@ cd D:\your-project-name\familyfund\rjsiao_family_fund
 REM Execute the Python script
 python review_mismatching.py
 
+REM Pause the execution so the user can see the results
+pause
+
 @REM TODO: 移除超過X日csv,txt舊檔案
